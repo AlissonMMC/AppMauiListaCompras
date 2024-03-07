9811110261d7f14b067d7a44a07361dcfdad6c02
Usuario <Usuario@CPD02-M05>
2024-03-07 15:07:15 -0300

    Finalizado pastas: Helper e views

--- AppMauiListaCompras/Helpers/SQLiteDatabaseHelper.cs
+++ AppMauiListaCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -33,9 +33,24 @@ namespace AppMauiListaCompras.Helpers
 
         }
 
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * from Produto WHERE " +
+                         "descricao LIKE '%" + q + "%";
+
+            return _conn.QueryAsync<Produto>(sql);
+        }
+
         public Task<int> Delete(int id)
         {
             return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
         }
+
+        
     }
 }
